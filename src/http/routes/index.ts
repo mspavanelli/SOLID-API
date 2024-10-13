24b512456f1ccaa49ee1f5ef55f0459402f8e67b
matheus.pavanelli <mspavanelli@gmail.com>
2024-10-12 22:24:05 -0300

    WIP: add auth

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from '@/http/controllers/authenticate'
 import { register } from '@/http/controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
